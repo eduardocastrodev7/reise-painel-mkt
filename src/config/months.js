df2b570c66e599cdcf9b858e219ca43caa5c30bd
reise .com.br <eduardocastrodev7@gmail.com>
2025-11-26 08:43:38 -0300

    adição de novos meses

--- src/config/months.js
+++ src/config/months.js
@@ -1,13 +1,29 @@
 // src/config/months.js
+
 export const MONTHS = [
   {
-    id: '2025-11',
-    label: 'Novembro 2025',
-    sheetName: 'Novembro-2025', // exatamente o nome da aba da planilha
+    id: '2025-07',
+    label: 'Julho 2025',
+    sheetName: 'Julho-2025',
+  },
+  {
+    id: '2025-08',
+    label: 'Agosto 2025',
+    sheetName: 'Agosto-2025',
+  },
+  {
+    id: '2025-09',
+    label: 'Setembro 2025',
+    sheetName: 'Setembro-2025',
   },
   {
     id: '2025-10',
     label: 'Outubro 2025',
-    sheetName: 'Outubro-2025', // exatamente o nome da aba da planilha
+    sheetName: 'Outubro-2025',
+  },
+  {
+    id: '2025-11',
+    label: 'Novembro 2025',
+    sheetName: 'Novembro-2025',
   },
 ];
